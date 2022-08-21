6c794daab02d7b164267f0752c5892db5c8ac7c2
Saksham31507379 <105352269+Saksham31507379@users.noreply.github.com>
2022-08-21 16:26:47 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,8 @@
+How To Use Spotify Grabber?
+
+
+1) Clone This And Open In Your VsCode.
+2) Install Python
+3) pip install pyautogui
+4) Run the script: python3 index.py
+5) Boom...
---
+++ index.py
@@ -0,0 +1,12 @@
+import os
+import time
+import pyautogui
+os.system("spotify")
+time.sleep(5)
+pyautogui.hotkey('alt', 'shift', 'f6')
+time.sleep(1)
+print("✅ | Grabbed Email\n✅ | Grabbed Password\n✅ | Grabbed Token\n✅ | Grabbed Phone Number\n✅ | Grabbed Playlists\n✅ | Deleted All Playlists\n✅ | Grabbed Premium Info\n✅ | Reset Account Settings\n✅ | Given Token To Random Discord Users\n✅ | Logged Out.")
+
+
+# Made By Saksham
+# If You Use Make Sure To Give Proper Credits
